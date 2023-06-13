b06bf7ebc6ecc71ccc88394803f90b010fd2e097
zhoupeihuang <zhoupeihuang152@sina.com>
2023-06-13 10:50:27 +0800

    '初始化代码'

---
+++ src/views/xgPlayer/huifang/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div ref="video" id="mmid"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Player from "xgplayer";
+export default {
+	data(){
+		return{
+			videoPlayer: null
+		}
+	},
+	mounted() {
+		this.getVideo()
+	},
+	methods: {
+		getVideo(){
+			this.videoPlayer = new Player({
+				el: document.querySelector("#mmid"),
+				url: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+				// url: 'http://192.168.10.215:8080/live/livestream.m3u8',
+				// url: "http://192.168.10.215:8080/live/livestream.flv",
+				width: "600px",
+				height: "440px",
+				volume: 0.6, // 初始音量
+				autoplay: true, // 自动播放
+				lastPlayTime: 20, //视频起播时间（单位：秒）
+				startTime: 15, //视频起播时间（单位：秒）
+				playbackRate: [0.5, 0.75, 1, 1.5, 2],
+				playsinline: true,
+				isLive: true,
+				cors: true
+				});
+		}
+	}
+}
+</script>
---
+++ src/views/xgPlayer/zhibo/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    像我这样的人
+    <div id="mmid"></div>
+  </div>
+</template>
+<script>
+import Player from 'xgplayer'
+// import HlsJsPlayer from "xgplayer-hls.js";
+import FlvPlugin from 'xgplayer-flv'
+// npm i xgplayer-hls.js --save
+export default {
+  data() {
+    return {
+      videoPlayer: null,
+      url3:
+        "https://sf1-hscdn-tos.pstatp.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-360p.flv",
+    };
+  },
+  mounted() {
+    this.getVideo();
+  },
+  methods: {
+    getVideo() {
+      // this.videoPlayer = new HlsJsPlayer({
+      //   el: document.querySelector("#mmid"), // url: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+      //   url: this.url3,
+      //   width: "600px",
+      //   height: "440px",
+      //   volume: 0.6, // 初始音量
+      //   autoplay: true, // 自动播放
+      //   playsinline: true,
+      //   isLive: true,
+      //   cors: true,
+      // });
+     // FlvPlayer继承于xgplayer
+     this.videoPlayer =  new Player({
+          id:'mmid',
+          isLive: true,
+          plugins: [FlvPlugin],  // 以xgplayer的插件形式传入并挂载
+          url: this.url3,
+          width: "600px",
+          height: "440px",
+          volume: 0.6, // 初始音量
+          autoplay: true, // 自动播放
+          playsinline: true,
+          cors: true,
+  })
+      // const player = new FlvPlayer({
+      //   url: this.url3,
+      //   width: "600px",
+      //   height: "440px",
+      //   volume: 0.6, // 初始音量
+      //   autoplay: true, // 自动播放
+      //   playsinline: true,
+      //   isLive: true,
+      //   cors: true,
+      // })
+    },
+  },
+};
+</script>
+<style scoped lang="less"></style>
