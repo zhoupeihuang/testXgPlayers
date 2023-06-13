fc1c2d902c3cfaeb3493619c6c387000528adadc
zhoupeihuang <zhoupeihuang152@sina.com>
2023-06-13 15:34:40 +0800

    '测试本地资源是否能播放'

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div> 
-    <video width="320" height="240" controls loop>
+    <video width="320" height="240" controls loop autoplay>
     <source :src="urlStr" type="video/mp4">
     <!-- <source src="movie.ogg" type="video/ogg"> -->
     您的浏览器不支持 video 标签。
@@ -11,7 +11,7 @@
 <script>
 import { computed, reactive,onMounted, onUnmounted ,ref, toRefs, watch} from 'vue'
 import { useRoute } from 'vue-router';
-
+import node1 from '../../assets/video/node1.mp4'
 export default {
   name:'',
   props: {
@@ -24,7 +24,7 @@ export default {
   },
   setup() {
     const route = useRoute()
-    let urlStr = route.query.url || "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+    let urlStr = route.query.url || node1 // "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
     
     return {urlStr}
   }
